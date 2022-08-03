771c3b43f097d161a5a3706cc6ae6dc2da5e9152
david <davidjhoyjr@gmail.com>
2022-08-02 21:29:35 -0400

    realtor show method

--- app/controllers/realtors_controller.rb
+++ app/controllers/realtors_controller.rb
@@ -1,7 +1,12 @@
 class RealtorsController < ApplicationController
+    skip_before_action :authenticate_realtor, only: [:create]
+    # # Above line needs to be in every controller 
+    # that we want to run that does not require authentication 
+
 
     def show
-       
+       realtor = Realtor.find(params[:id])
+       render json: realtor, status: :ok
     end
 
     def create
