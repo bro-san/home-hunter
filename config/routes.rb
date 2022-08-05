aa7bc7186435994dfb4d83760f713d6c2f0b3669
justin.hobbs <justinhobbs@fastmail.com>
2022-08-04 21:18:00 -0500

    testing heroku packages

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  resources :realtors
-  resources :home_buyers
-  resources :requests
-  resources :responses
+  resources :realtors, only: [:index, :show, :create]
+  resources :home_buyers, only: :index
+  resources :requests, only: [:index, :show, :create, :destroy, :updated]
+  resources :responses, only: :index
 
   post "/login", to: "sessions#create"
 
