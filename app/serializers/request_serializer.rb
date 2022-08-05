14667ff2d5c57374746ba7809711f47cab31c0f6
justin.hobbs <justinhobbs@fastmail.com>
2022-08-04 21:35:11 -0500

    added request serializer

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,3 +1,4 @@
 class RequestSerializer < ActiveModel::Serializer
   attributes :id, :home_buyer_id, :location_specific, :location_size, :comment, :wish1, :wish2, :wish3
+  belongs_to :home_buyer
 end
